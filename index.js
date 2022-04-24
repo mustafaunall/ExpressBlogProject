ac34fad433376da58fa945baf350eb2f7e5b5ef8
Mustafa Ünal <revengefiretr@gmail.com>
2022-04-24 19:22:23 +0300

    model files added

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const express = require('express')
 require('dotenv').config()
+const { PORT, MONGO_CONNECTION } = process.env
+const express = require('express')
+const mongoose = require('mongoose')
+const expressHBS = require('express-handlebars')
 
 const app = express()
-const { PORT } = process.env
 
-
-app.listen(3000, () => {
-    console.log(`Application started on http://localhost:${PORT}`)
+const hbs = expressHBS.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
 })
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+// app.use -> customRoute
+
+const start = async () => {
+    try {
+        await mongoose.connect(MONGO_CONNECTION, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        app.listen(PORT, async () => {
+            console.log(`Application started on http://localhost:${PORT}`)
+        })
+    } catch (error) {
+        console.log('ERROR', error)
+    }
+}
+
+start()
