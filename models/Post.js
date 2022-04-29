9e2ebb1c50894712e4620659d147084d9f4f253e
Mustafa Ünal <revengefiretr@gmail.com>
2022-04-29 16:36:40 +0300

    post schema added

---
+++ models/Post.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose')
+
+const Post = Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: false, default: 0 },
+    isSelled: { type: Boolean, required: false, default: false },
+    isActive: { type: Boolean, required: false, default: true },
+})
+
+module.exports = model('Post', Post)
